17161c39816d7f61a944d282353530c6a20a3a4b
Joona <108073590+Jobe2000@users.noreply.github.com>
2023-07-03 12:23:59 +0300

    updated my-cat.c

--- Project 2 - Unix Utilities/my-cat.c
+++ Project 2 - Unix Utilities/my-cat.c
@@ -16,13 +16,8 @@ size_t bufsize = 0;
 // Read the input file and writes it reversed to the output file
 int read_file (FILE * input) {
 
-    char *buffer;
-    if((buffer = (char *)malloc(bufsize * sizeof(char))) == NULL)
-    {
-        fprintf(stderr, "malloc failed\n");
-        exit(1);
-    }
-
+    char *buffer = NULL;
+    
     while (1) {
         size_t characters = getline(&buffer, &bufsize, input);
 
@@ -32,13 +27,15 @@ int read_file (FILE * input) {
         }
         printf("%s", buffer);
     }
+    free(buffer);
+
     return(0);
 }
 
 FILE * open_file(char * file_name, char * mode) {
     FILE * file = NULL;
     if((file = fopen(file_name, mode)) == NULL) {
-        fprintf(stderr, "error: cannot open file '%s'\n", file_name);
+        fprintf(stderr, "my-cat: cannot open file");
         exit(1);
     }
     return file;
@@ -48,8 +45,7 @@ int main (int argc, char * argv[]) {
     
     if(argc == 1)
     {
-        fprintf(stderr, "Needs at least one input file.\n");
-        exit(1);
+        exit(0);
     }
 
     for(int i = 1; i < argc; i++)
