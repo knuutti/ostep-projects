ccdbcc2c28b42254f266356e03f1487b4e13874c
Knuutti <96994953+Knuutti@users.noreply.github.com>
2023-07-10 11:56:16 +0300

    Väli push

--- Project 5 - Parallel Zipping/pzip.c
+++ Project 5 - Parallel Zipping/pzip.c
@@ -2,16 +2,46 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <math.h>
 
 typedef struct thread_args {
     int index;
+    int n_thread;
 } thread_args;
 
 // The function to be executed by all threads
 void * parallelZip(void * args) { 
-    
+
     thread_args * actual_arguments = args;
-    printf("Number: %d\n", actual_arguments->index);
+
+    size_t bufsize = 0;
+    FILE * input = NULL;
+    if((input = fopen("ksii.txt", "r")) == NULL) {
+        fprintf(stderr, "error: cannot open file\n");
+        exit(1);
+    }
+
+    fseek(input, 0, SEEK_END);
+    int file_size = ftell(input);
+    int seg_size = round(file_size / actual_arguments->n_thread);
+    printf("%d, %d\n", file_size, seg_size);
+    fseek(input, seg_size * actual_arguments->index, SEEK_SET);
+
+    int current = -1;
+    int temp;
+    int counter;
+    while(1) {
+        temp = fgetc(input);
+        printf("%c", temp);
+        //fwrite(&current, sizeof(char), 1, stdout);
+        if (feof(input) || ftell(input) == seg_size * (actual_arguments->index + 1)) {
+            break;
+        }
+    }
+
+    fclose(input);
+
+    //printf("Number: %d\n", actual_arguments->index);
 
 }
 
@@ -53,6 +83,8 @@ int main(int argc, char * argv[])
 
         printf("Loop %d\n", i+1);
         args->index = i;
+        args->n_thread = threads;
+        sleep(1);
 
         pthread_create(&tid, NULL, parallelZip, args);
 
