7dd13cde1ab4b5c2b3b4d5f61703c0eaf3d76cab
Knuutti <96994953+Knuutti@users.noreply.github.com>
2023-07-10 11:20:17 +0300

    Project 5 initial commit

--- Project 5 - Parallel Zipping/pzip.c
+++ Project 5 - Parallel Zipping/pzip.c
@@ -3,51 +3,62 @@
 #include <unistd.h>
 #include <pthread.h>
 
+typedef struct thread_args {
+    int index;
+} thread_args;
+
 // The function to be executed by all threads
-void *myThreadFun(void *vargp)
-{
-    // Store the value argument passed to this thread
-    int *myid = (int *)vargp;
-  
-    // Variable for tracking the thread number
-    static int i = 0;
-    i++;
+void * parallelZip(void * args) { 
+    
+    thread_args * actual_arguments = args;
+    printf("Number: %d\n", actual_arguments->index);
 
-    //fseek(input, 0, SEEK_CUR);
-
-    // Print the argument, static and global variables
-    printf("Thread ID: %d, Thread number: %d\n", *myid, i);
 }
 
-int zip(FILE * input, int procs, int segs) {
+// int zip(FILE * input, int procs, int segs) {
 
-    int i;
-    pthread_t tid;
+//     int i;
+//     pthread_t tid;
 
-    fseek(input, 0, SEEK_END);
-    int file_size = ftell(input);
-    fseek(input, 0, SEEK_SET);
+//     fseek(input, 0, SEEK_END);
+//     int file_size = ftell(input);
+//     fseek(input, 0, SEEK_SET);
 
-    printf("%d\n", file_size);
+//     printf("Size: %d, Threads: %d\n", file_size, procs);
+
+//     thread_args *args = malloc(sizeof(thread_args));
   
-    for (i = 0; i < procs; i++)
-        pthread_create(&tid, NULL, myThreadFun, (void *)&tid);
-  
-    pthread_exit(NULL);
+//     for (i = 0; i < procs; i++)
 
-    return(0);
-}
+//         args->index = i;
+//         args->file = input;
+//         printf("123123\n",i);
+
+//         pthread_create(&tid, NULL, parallelZip, args);
+  
+//     pthread_exit(NULL);
+
+//     return(0);
+// }
   
 int main(int argc, char * argv[])
 {
 
-    FILE * input = fopen(argv[1], "r");
-    int procs = get_nprocs_conf();
-    int segs = procs - 1;
+    pthread_t tid;
+    int threads = get_nprocs_conf() - 1;
 
-    zip(input, procs, segs);
+    thread_args * args = malloc(sizeof *args);
+  
+    for (int i = 0; i < threads; i++) {
 
-    fclose(input);
+        printf("Loop %d\n", i+1);
+        args->index = i;
+
+        pthread_create(&tid, NULL, parallelZip, args);
+
+    }
+  
+    pthread_exit(NULL);
 
     return 0;
 }
