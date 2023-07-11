06b8c0e8172e65751dfc0ad74c76d3dd1f92902e
Knuutti <96994953+Knuutti@users.noreply.github.com>
2023-07-11 16:49:00 +0300

    Added documentation

--- Project 1 - Warmup to C and Unix programming/reverse.c
+++ Project 1 - Warmup to C and Unix programming/reverse.c
@@ -2,6 +2,7 @@
 CT30A3370 Käyttöjärjestelmät ja systeemiohjelmointi
 Project 1: Warmup to C and Unix programming
 
+File: reverse.c
 Authors: Eetu Knutars & Joona Lappalainen
 Last modified: 11.7.2023
 */
@@ -27,40 +28,47 @@ size_t bufsize = 0;
 // Reverse function: reads the input file and writes it reversed to the output file
 int reverse (FILE * input, FILE * output) {
 
+    // Defining and allocating memory for buffer, which used for
+    // reading file with getline()
     char *buffer;
-    /* Allocate memory */
     if((buffer = (char *)malloc(bufsize * sizeof(char))) == NULL)
     {
         fprintf(stderr, "malloc failed\n");
         exit(1);
     }
 
+    // While-loop for reading the input file
+    // If input is read from stdin, user can end the read by pressing Ctrl+D
     while (1) {
+
+        // Read a line and store it to the buffer pointer, also keeping
+        // track of the character count
         size_t characters = getline(&buffer, &bufsize, input);
 
-        // Stop reading input on file end
-        // If input is read from stdin, user can end the read by pressing Ctrl+D
+        // Stop reading input at EOF
         if (characters == -1) {
             break;
         }
-        /* Allocate memory */
+        // Allocating memory for the new list node
         if ((pNew = (ROWS*)malloc(sizeof(ROWS))) == NULL ){
             fprintf(stderr, "malloc failed\n");
             exit(1);
         }
-        /* Allocate memory */
         if ((pNew->row = (char*)malloc(bufsize)) == NULL ){
             fprintf(stderr, "malloc failed\n");
             exit(1);
         }
 
+        // Storing lines to linked list
         strcpy(pNew->row, buffer); 
         pNew->next = NULL;
 
-        if (pStart == NULL) { /* If list is empty */
+        // If list is empty, initialize pStart and pEnd
+        if (pStart == NULL) { 
             pStart = pNew;
             pEnd = pNew;
         } 
+        // Update next and previous nodes
         else { 
             pEnd->next = pNew;
             pNew->prev = pEnd;
@@ -68,13 +76,15 @@ int reverse (FILE * input, FILE * output) {
         }
 
     }
-    /* Reversed writing */
+    // Writing reversed: start the linked list from pEnd, use the
+    // attribute prev to move to the previous line
     ptr = pEnd;
     while (ptr != NULL) {
         fprintf(output, "%s", ptr->row);
         ptr = ptr->prev;
     }
-    /* Free memory */
+    
+    // Free the linked list after writing
     ptr = pStart;
     while (ptr != NULL) {
         pStart = ptr->next;
@@ -84,14 +94,17 @@ int reverse (FILE * input, FILE * output) {
     
     return(0);
 }
-/* Opening file with error handler */
+
+// Function for opening file with error handler
 FILE * open_file(char * file_name, char * mode) {
+
     FILE * file = NULL;
     if((file = fopen(file_name, mode)) == NULL) {
         fprintf(stderr, "error: cannot open file '%s'\n", file_name);
         exit(1);
     }
-    return file;
+
+    return(file);
 }
 
 int main (int argc, char * argv[]) {
